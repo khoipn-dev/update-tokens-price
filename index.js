81e358a360eefa8442acf380a936ba13d8f40491
KhoiPN <khoipn.dev@gmail.com>
2021-05-23 14:35:52 +0700

    Update bot

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const ABI = require("./abi/pancake_pair_abi.json");
 const { web3 } = require("./web3");
 const { Telegraf } = require("telegraf");
-const axios = require("axios");
+const Binance = require("node-binance-api");
 
 const XPO_BUSD_PAIR_ADDRESS = "0xdf1bc24552c04c9e0a3cf09b4148ea438087181e";
 const XPO_TOKEN_ADDRESS = "0xeBB59CeBFb63f218db6B5094DC14AbF34d56D35D";
+const BSCX_BUSD_PAIR_ADDRESS = "0xA32A983a64ce21834221AA0AD1f1533907553136";
+const BSCX_TOKEN_ADDRESS = "0x5ac52ee5b2a633895292ff6d8a89bb9190451587";
 const BOT_TELEGRAM_TOKEN = "1780315149:AAFmkfg1dkEwKSfO1ypMIaWu_FChx3UPm6g";
-const CHANNEL_ID = "@xpo_price";
-const TIME_ONCE_UPDATE = 15 * 1000;
+const CHANNEL_ID = "@khoipn_crypto_bot";
+const TIME_ONCE_UPDATE = 10 * 1000;
 const MY_ADDRESS = '0xC00Ece9f495Cb089DF429f383d3e164AC6bC198c';
 
+const binance = new Binance();
 const bot = new Telegraf(BOT_TELEGRAM_TOKEN);
 
 setInterval(async () => {
@@ -21,15 +24,15 @@ setInterval(async () => {
 }, TIME_ONCE_UPDATE);
 
 const updatePrice = async () => {
-  const xpoPrice = await getPriceXPO(XPO_BUSD_PAIR_ADDRESS);
-  const xpoBalance = await getBalanceOfAddress(XPO_TOKEN_ADDRESS, MY_ADDRESS);
-  const totalXPO = xpoPrice * xpoBalance;
+  const priceXPO = await getTokenPrice(XPO_BUSD_PAIR_ADDRESS);
+  const balanceXPO = await getBalanceOfAddress(XPO_TOKEN_ADDRESS, MY_ADDRESS);
+  const totalXPO = priceXPO * balanceXPO;
   const bnbPrice = await getPriceBNB();
   const bnbBalance = await getBalanceOfAddress(null, MY_ADDRESS);
   const totalBNB = bnbPrice * bnbBalance;
   bot.telegram.sendMessage(
     CHANNEL_ID,
-    `⚡${xpoPrice.toFixed(4)}⚡${totalXPO.toFixed(4)}⚡${bnbPrice}⚡${totalBNB.toFixed(4)}⚡`
+    `${priceXPO.toFixed(3)}⚡${totalXPO.toFixed(3)}⚡${bnbPrice}⚡${totalBNB.toFixed(3)}`
   );
 };
 
@@ -44,7 +47,7 @@ const getBalanceOfAddress = async (contractAddress, address) => {
   }
 };
 
-const getPriceXPO = async (pairAddress) => {
+const getTokenPrice = async (pairAddress) => {
   const pair_contract = new web3.eth.Contract(ABI, pairAddress);
   const reserve = await pair_contract.methods.getReserves().call();
   const raw_amount_0 = reserve._reserve0;
@@ -59,8 +62,8 @@ const getPriceXPO = async (pairAddress) => {
 
 const getPriceBNB = async () => {
   try {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=binancecoin');
-   return res.data[0].current_price || 0;
+    let ticker = await binance.prices("BNBBUSD") || 0;
+    return Number(ticker["BNBBUSD"]).toFixed(3);
   } catch {
     return 0;
   }
